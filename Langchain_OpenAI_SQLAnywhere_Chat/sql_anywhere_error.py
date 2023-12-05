f67dd854b1da6098be668a94ce20cebea5600a4c
learn360sujeet <121271779+learn360sujeet@users.noreply.github.com>
2023-12-04 19:17:06 -0600

    testing

--- Langchain_OpenAI_SQLAnywhere_Chat/sql_anywhere_error.py
+++ Langchain_OpenAI_SQLAnywhere_Chat/sql_anywhere_error.py
@@ -7,6 +7,7 @@ from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain.sql_database import SQLDatabase
 from langchain.llms.openai import OpenAI
 from langchain.agents import AgentExecutor
+from sqlalchemy import create_engine
 
 # Database connection details for Microsoft SQL Server
 DATABASE_SERVER = 'sqldemo'
@@ -14,14 +15,25 @@ DATABASE_NAME = 'demo'
 DATABASE_USERNAME = 'dba'
 DATABASE_PASSWORD = 'sql'
 DRIVER = '{SQL Anywhere 17}'
+PORT=2638 
+DATABASE_HOST = 'localhost'
 
 
 def create_agent_executor():
    
-    conn_uri = f"sqlanywhere+pyodbc://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_SERVER}/{DATABASE_NAME}?driver=SQL+Anywhere+17"  
+   # engine = create_engine(f'sqlanywhere+pyodbc://{DATABASE_USERNAME}:{DATABASE_USERNAME}@{DATABASE_SERVER}:{PORT}/{DATABASE_NAME}?driver=sql+anywhere+17')
     
-    #error line
-    db = SQLDatabase.from_uri(conn_uri) #error line 
+    # Create the SQLAlchemy engine using the SQL Anywhere dialect
+    
+    engine = create_engine(f'sqlalchemy_sqlany://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}')
+    db = SQLDatabase(engine) #error line 
+    
+    # conn_uri = f"sqlalchemy_sqlany://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"      
+    # db = SQLDatabase.from_uri(conn_uri) #error line 
+    
+
+
+
 
     # Instantiate your language model here
     llm = OpenAI(api_key=os.environ['OPENAI_API_KEY'], temperature=0)
